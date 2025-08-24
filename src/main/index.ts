4827d323a88f29a36e01e55898291d7720a5f0ba
Karine91 <26932594+Karine91@users.noreply.github.com>
2025-08-24 17:53:27 +0200

    upgrade deps

--- src/main/index.ts
+++ src/main/index.ts
@@ -24,6 +24,8 @@ const createWindow = (): void => {
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
       nodeIntegration: false,
+      contextIsolation: true,
+      nodeIntegrationInWorker: true,
     },
   });
 
@@ -40,10 +42,6 @@ const reactDevToolsPath = "D://Projects/ReactDevTools";
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on("ready", async () => {
-  if (isDevelopment) {
-    await session.defaultSession.loadExtension(reactDevToolsPath);
-  }
-
   handleSystemCommands();
   handleApi();
   createWindow();
--- src/renderer/theme.ts
+++ src/renderer/theme.ts
@@ -1,11 +1,6 @@
 import { extendTheme } from "@chakra-ui/react";
-import chakraTheme from "@chakra-ui/theme";
-const { Button } = chakraTheme.components;
 
 const theme = extendTheme({
-  components: {
-    Button,
-  },
   styles: {
     global: {
       "html, body": {
